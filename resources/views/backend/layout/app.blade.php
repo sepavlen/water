832df5cb8af8dfb767831f92bc3d25f38146f5cc
sepavlen <sepavlen@gmail.com>
2021-02-28 23:14:49 +0200

    clear proj

--- resources/views/backend/layout/app.blade.php
+++ resources/views/backend/layout/app.blade.php
@@ -9,24 +9,14 @@
     <meta content="" name="author" />
     
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
-    
-    
-    <link href="/assets/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/morris/morris.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/jqvmap/jqvmap/jqvmap.css" rel="stylesheet" type="text/css" />
-    
+    <link href="/assets/global/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/global/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/global/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     
     <link href="/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
     <link href="/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
-    
-    
+
+
     <link href="/assets/layouts/layout/css/layout.min.css" rel="stylesheet" type="text/css" />
     <link href="/assets/layouts/layout/css/themes/darkblue.min.css" rel="stylesheet" type="text/css" id="style_color" />
     <link href="/assets/layouts/layout/css/custom.min.css" rel="stylesheet" type="text/css" />
@@ -320,83 +310,15 @@
     </div>
 </div>
 
-{{--<!--[if lt IE 9]>--}}
-{{--<script src="/assets/global/plugins/respond.min.js"></script>--}}
-{{--<script src="/assets/global/plugins/excanvas.min.js"></script>--}}
-{{--<![endif]-->--}}
-
-<script src="/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-{{--<script src="/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>--}}
-{{----}}
-{{----}}
-
-<script src="/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
-
-
-{{--<script src="/assets/global/scripts/datatable.js" type="text/javascript"></script>--}}
-<script src="/assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
-
-
-<script src="/assets/global/scripts/app.min.js" type="text/javascript"></script>
-
-
-<script src="/assets/pages/scripts/table-datatables-managed.min.js" type="text/javascript"></script>
-
-
-
-{{--<script src="/assets/global/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/morris/morris.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/morris/raphael-min.js" type="text/javascript"></script>--}}
-<script src="/assets/global/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/amcharts/amcharts/amcharts.js" type="text/javascript"></script>
-{{--<script src="/assets/global/plugins/amcharts/amcharts/serial.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/amcharts/pie.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/amcharts/radar.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/amcharts/themes/light.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/amcharts/themes/patterns.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/amcharts/themes/chalk.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/ammap/ammap.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/ammap/maps/js/worldLow.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/amcharts/amstockcharts/amstock.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>--}}
-{{----}}
-{{----}}
-<script src="/assets/global/scripts/app.min.js" type="text/javascript"></script>
-
-
-{{--<script src="/assets/pages/scripts/dashboard.min.js" type="text/javascript"></script>--}}
-
-
+<script src="/assets/global/jquery.min.js" type="text/javascript"></script>
+<script src="/assets/global/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/assets/global/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="/assets/global/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+<script src="/assets/global/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+<script src="/assets/global/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="/assets/global/app.min.js" type="text/javascript"></script>
 <script src="/assets/layouts/layout/scripts/layout.min.js" type="text/javascript"></script>
-{{--<script src="/assets/layouts/layout/scripts/demo.min.js" type="text/javascript"></script>--}}
-{{--<script src="/assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>--}}
-{{----}}
+
 </body>
 
 </html>
