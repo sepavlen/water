3321a35a2a3052213af3381d02d9e9838fcca0b0
sepavlen <sepavlen@gmail.com>
2021-03-04 00:24:37 +0200

    qeqwe

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('status')->default(1)->comment('1 - Active, 2 - Blocked');
