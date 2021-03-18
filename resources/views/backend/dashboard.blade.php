5456f13e6225e1759d69a8d9491175b478036633
sepavlen <sepavlen@gmail.com>
2021-03-19 00:04:53 +0200

    add chart

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,5 +1,44 @@
 @extends('backend.layout.app')
+<style>
+    #container {
+        height: 420px;
+    }
 
+    .highcharts-figure, .highcharts-data-table table {
+        min-width: 360px;
+        /*max-width: 820px;*/
+        margin: 1em auto;
+    }
+
+    .highcharts-data-table table {
+        font-family: Verdana, sans-serif;
+        border-collapse: collapse;
+        border: 1px solid #EBEBEB;
+        margin: 10px auto;
+        text-align: center;
+        width: 100%;
+        max-width: 500px;
+    }
+    .highcharts-data-table caption {
+        padding: 1em 0;
+        font-size: 1.2em;
+        color: #555;
+    }
+    .highcharts-data-table th {
+        font-weight: 600;
+        padding: 0.5em;
+    }
+    .highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+        padding: 0.5em;
+    }
+    .highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+        background: #f8f8f8;
+    }
+    .highcharts-data-table tr:hover {
+        background: #f1f7ff;
+    }
+
+</style>
 @section('content')
 
                 <div class="page-bar">
@@ -77,6 +116,9 @@
                     </div>
                 </div>
                 <div class="clearfix"></div>
+                <figure class="highcharts-figure">
+                    <div id="container"></div>
+                </figure>
 
 @endsection
 @if (session('status'))
@@ -84,3 +126,126 @@
         {{ session('status') }}
     </div>
 @endif
+@push('scripts')
+    <script>
+        var colors = Highcharts.getOptions().colors;
+        Highcharts.chart('container', {
+            chart: {
+                type: 'spline'
+            },
+
+            legend: {
+                symbolWidth: 40
+            },
+
+            title: {
+                text: 'Доход и затраты за последние 30 дней'
+            },
+
+            yAxis: {
+                title: {
+                    text: false
+                }
+            },
+
+            xAxis: {
+                title: {
+                    text: 'ДД.ММ'
+                },
+                categories: [
+                    '18.02',
+                    '19.02',
+                    '20.02',
+                    '21.02',
+                    '22.02',
+                    '23.02',
+                    '24.02',
+                    '25.02',
+                    '26.02',
+                    '27.02',
+                    '28.02',
+                    '01.03',
+                    '01.03',
+                    '03.03',
+                    '04.03',
+                    '05.03',
+                    '06.03',
+                    '07.03',
+                    '08.03',
+                    '09.03',
+                    '10.03',
+                    '11.03',
+                    '12.03',
+                    '13.03',
+                    '14.03',
+                    '15.03',
+                    '16.03',
+                    '17.03',
+                    '18.03',
+                    '19.03',
+                ]
+            },
+
+            plotOptions: {
+                series: {
+                    states: {
+
+                        inactive: {
+                            opacity: 1
+                        }
+
+                    },
+                    cursor: 'pointer',
+                },
+
+            },
+            credits: {
+                text: 'Water',
+                href: false,
+            },
+            tooltip: {
+                valueSuffix: ' грн.'
+            },
+            exporting: {
+                buttons: {
+                    contextButton: {
+                        menuItems: ['downloadPNG', 'downloadPDF', 'downloadXLS']
+                    }
+                }
+            },
+
+            series: [
+                {
+                    name: 'Доход',
+                    data: [ 533, 443, 451, 414,449, 524, 533, 443, 451, 394,449, 524,  533, 443, 451, 414,449, 524, 533, 343, 451, 414,449, 524, 533, 443, 451, 364,449, 524, ],
+                    color: colors[2],
+                }, {
+                    name: 'Затраты',
+                    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 850, 0, 0, 0, 0, 0, 0, 0],
+                    color: colors[3],
+                    dashStyle: 'ShortDash',
+                }
+            ],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 550
+                    },
+                    chartOptions: {
+                        chart: {
+                            spacingLeft: 3,
+                            spacingRight: 3
+                        },
+                        legend: {
+                            itemWidth: 150
+                        },
+                        yAxis: {
+                            visible: false
+                        }
+                    }
+                }]
+            }
+        });
+    </script>
+@endpush
--- resources/views/backend/layout/app.blade.php
+++ resources/views/backend/layout/app.blade.php
@@ -249,7 +249,13 @@
 <script src="/assets/global/datatables.min.js" type="text/javascript"></script>
 <script src="/assets/global/datatables.bootstrap.js" type="text/javascript"></script>
 <script src="/assets/global/table-datatables-managed.js" type="text/javascript"></script>
-
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/series-label.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script src="https://code.highcharts.com/themes/high-contrast-light.js"></script>
+@stack('scripts')
 </body>
 
 </html>
